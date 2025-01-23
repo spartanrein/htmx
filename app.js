9013bb0abdd8230124550692ad514de7c8db6593
spartanrein <37677855+spartanrein@users.noreply.github.com>
2025-01-23 11:40:55 +0800

    create get request by clicking button

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
         <main>
           <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <button hx-get="/info">Learn More</button>
+          <button hx-get="/info" hx-swap="outerHTML>Learn More</button>
         </main>
       </body>
     </html>
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 app.get('/info', (req, res) => {
   res.send(`
     <ul>
-      ${HTMX_KNOWLEDGE.map(info => `<li>${info}</li>`)}
+      ${HTMX_KNOWLEDGE.map(info => `<li>${info}</li>`).join('')}
     </ul>
   `);
 });
